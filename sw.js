0e44657a9cd4ceaec2f78d16a186c0b95caa771b
mixermallows <mix.keymescolare@gmail.com>
2026-08-05 18:28:52 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,28 @@
-const CACHE_NAME = 'cashforecast-v2'; // เปลี่ยนจาก v1 เป็น v2
+const CACHE_NAME = 'cashforecast-v3';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './manifest.json'
+  './manifest.json',
+  './logo.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // บังคับให้ใช้ Service Worker ตัวใหม่ทันที
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// เพิ่มส่วนนี้เพื่อบังคับลบแคชเวอร์ชันเก่าทั้งหมดทิ้ง
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
+            return caches.delete(cacheName); // ลบแคชเก่าทิ้งทั้งหมด
           }
         })
       );
